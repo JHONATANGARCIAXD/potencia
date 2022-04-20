d212d9df3d08da1b7168f95d4ab6821a6d7a8aeb
Jhonatan <jhonatangarcia@colegioguanenta.edu.co>
2022-04-20 07:56:12 -0500

    Desarrollo: programa terminadoen python. Jhonatan

---
+++ README.md
@@ -0,0 +1,5 @@
+# Programa No. 2
+
+### Programa para calcular x a la y.
+
+![Diagarama de flujo](diagrama.png "Diagrama de flujo"  )
---
+++ potencia.py
@@ -0,0 +1,17 @@
+"""Progema No. 2
+Programa para calcular x a la y"""
+
+print("------------------------------")
+print("-----Potencia x a la y---------")
+print("------------------------------")
+
+# imput
+x=int(input("Digite el valor de x: "))
+y=int(input("Digite el valor de y: "))
+
+#procesing
+z= x**y
+
+# output
+print("Resultados:\n")
+print(str(x) + " a la " + str(y)  +  " es igual a "  + str(z))
